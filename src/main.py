42c805f077ea8ac2a659f5257bc4d19ccf82a37d
massan11 <mhassan.zoho@gmail.com>
2025-03-14 17:31:04 +0330

    Import Book model in main.py for database table creation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from src.routes.book_routes import book_routes, user_routes
 from src.database import engine, Base
+from src.models.book_models import Book  # Import the Book model
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
